56f03e72ea0bba4eafe6126ac254a34a1c4e96bb
Carlos Vaz <carlosjvaz@gmail.com>
2020-03-23 12:18:28 -0300

    Updates

---
+++ precache-manifest.c8a4f983dbc17229fea441d4a5470c16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29fe8e94ab4425e2d8d6c1307c11730b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8de9108aabb850f2c15e",
+    "url": "/monsters-rolodex/static/css/main.39718c4f.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de9108aabb850f2c15e",
+    "url": "/monsters-rolodex/static/js/main.f5806d8b.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
